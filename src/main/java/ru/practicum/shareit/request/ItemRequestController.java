036105e10dcb33484e19c1c8e089550e9296f2aa
Ruslan Sabirov <r.sabirov@wisetech.pro>
2023-07-18 16:45:54 +0300

    feat: Добавить ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,62 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ResponseItemRequestDto;
+import ru.practicum.shareit.request.service.IremRequestServiceImpl;
+import ru.practicum.shareit.utils.Messages;
+import ru.practicum.shareit.validation.marker.Create;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+import static ru.practicum.shareit.item.ItemController.USER_ID_HEADER;
+import static ru.practicum.shareit.utils.Constants.DEFAULT_FROM_VALUE;
+import static ru.practicum.shareit.utils.Constants.DEFAULT_SIZE_VALUE;
+
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/requests")
+@Validated
 public class ItemRequestController {
+
+    private final IremRequestServiceImpl itemRequestService;
+
+    @PostMapping
+    public ResponseItemRequestDto add(@Validated({Create.class}) @RequestBody ItemRequestDto requestDto,
+                                      @RequestHeader("X-Sharer-User-Id") int requesterId) {
+        log.info(Messages.addItemRequest(requesterId));
+        ItemRequest request = ItemRequestMapper.toItemRequest(requestDto);
+        request = itemRequestService.createItemRequest(request, requesterId);
+        return ItemRequestMapper.toResponseItemRequestDto(request);
+    }
+
+    @GetMapping
+    public List<ResponseItemRequestDto> getAllByOwner(@RequestHeader(USER_ID_HEADER) int requesterId) {
+        log.info(Messages.getItemRequestsForOwner(requesterId));
+        return itemRequestService.getForOwner(requesterId);
+    }
+
+    @GetMapping("/all")
+    public List<ResponseItemRequestDto> getAll(@RequestParam(defaultValue = DEFAULT_FROM_VALUE)
+                                               @Min(0) int from,
+                                               @RequestParam(defaultValue = DEFAULT_SIZE_VALUE)
+                                               @Positive int size,
+                                               @RequestHeader(USER_ID_HEADER) int userId) {
+        log.info(Messages.getAllRequestForUser(userId));
+        return itemRequestService.findAll(from, size, userId);
+    }
+
+    @GetMapping("/{requestId}")
+    public ResponseItemRequestDto getById(@PathVariable int requestId,
+                                          @RequestHeader(USER_ID_HEADER) int userId) {
+        log.info(Messages.getRequestById(requestId, userId));
+        return itemRequestService.findById(requestId, userId);
+    }
+
 }
