fdae98b218cb198a129228938b0ec56a95f1d0c7
Ruslan Sabirov <r.sabirov@wisetech.pro>
2023-08-04 16:03:20 +0300

    style

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -18,6 +18,7 @@ import ru.practicum.shareit.validation.marker.Update;
 public class UserController {
 
     private final UserClient userClient;
+
     @GetMapping("{id}")
     public ResponseEntity<Object> get(@PathVariable int id) {
         log.info(Messages.getUser(id));
@@ -38,7 +39,7 @@ public class UserController {
 
     @PatchMapping("{id}")
     public ResponseEntity<Object> update(@Validated({Update.class}) @RequestBody UserDto userDto,
-                          @PathVariable("id") int id) {
+                                         @PathVariable("id") int id) {
         log.info(Messages.updateUser(id));
         return userClient.update(userDto, id);
     }
